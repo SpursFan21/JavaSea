999f20a070ef58054aed7d66f0eeb6de14ddfae4
SpursFan21 <duncanfrederic@gmail.com>
2024-08-30 18:24:14 +1200

    more

--- Shape/my-app/src/main/java/com/example/App.java
+++ Shape/my-app/src/main/java/com/example/App.java
@@ -15,6 +15,15 @@ public class App
         int b = f(a * a);
         return b;
     }
+    int ff(int x1) {
+        return x1 * 2 - 1;
+      }
+      
+      int h() {
+        int a1 = 3;
+        int b1 = ff(a1) + ff(4);
+        return b1;
+      }
     public static void main( String[] args )
     {
         App app = new App();
@@ -22,9 +31,13 @@ public class App
         int resultMyfunction = app.myFunction(3, 7);
         int resultF = app.f(5);
         int resultG = app.g();
+        int resultFF = app.ff(3);
+        int resultH = app.h();
 
         System.out.println("Result of myFunction(3,7): " + resultMyfunction);
         System.out.println("Result of f(5): " + resultF);
         System.out.println("Result of g(): " + resultG);
+        System.out.println("Result of ff: " + resultFF);
+        System.out.println("Result of h: " + resultH);
     }
 }
